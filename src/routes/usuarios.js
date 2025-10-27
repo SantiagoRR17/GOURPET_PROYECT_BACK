37ace11001451a71bbfcf587dc1474de47e48a45
SantiagoRR17 <santiagorochar.03@gmail.com>
2025-10-26 23:29:39 -0500

    se montan las funciones get y create de usuarios, queda pendiente pruebas de los metodos mencionados con POSTMAN

---
+++ src/routes/usuarios.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router(); //manejador de rutas de express
+const usuarioSchema = require("../models/usuario");
+//Nuevo usuario
+router.post("/usuarios", (req, res) => {
+ const user = usuarioSchema(req.body);
+ user
+ .save()
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error }));
+
+});
+
+//Consultar todos los usuarios
+router.get("/usuarios", (req, res) => {
+ usuarioSchema.find() //Aqui se ponen los filtros de las especificas
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error }));
+});
+
+//Consultar un usuario por su id
+router.get("/usuarios/:id", (req, res) => {
+ const { id } = req.params;
+ usuarioSchema
+ .findById(id)
+ .then((data) => res.json(data))
+ .catch((error) => res.json({ message: error }));
+});
+
+//Consultar cantidad de usuarios registrados
+router.get("/usuarios/count", (req, res) => {
+ usuarioSchema.countDocuments()
+ .then((count) => res.json({ count }))
+ .catch((error) => res.json({ message: error }));
+});
+
+
+
+
+module.exports = router;
