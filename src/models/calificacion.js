52508e0a0cb4da058c3aceea85e3b9e5c2e8b277
Julian Avila <julian.avila.967@gmail.com>
2025-11-02 00:51:10 -0500

    Se elimina la opcion de calificar varias veces una receta

--- src/models/calificacion.js
+++ src/models/calificacion.js
@@ -27,20 +27,7 @@ const calificacionSchema = new mongoose.Schema({
   }
 });
 
-/* Evitar que un mismo usuario califique la misma receta más de una vez
-calificacionSchema.index({ usuario: 1, receta: 1 }, { unique: true });
 
- Middleware opcional: actualizar promedio de la receta cuando se crea o modifica una calificación
-calificacionSchema.post("save", async function () {
-  const Receta = mongoose.model("Receta");
-  const Calificacion = mongoose.model("Calificacion");
 
-  const calificaciones = await Calificacion.find({ receta: this.receta });
-  if (calificaciones.length > 0) {
-    const promedio =
-      calificaciones.reduce((acc, c) => acc + c.puntaje, 0) / calificaciones.length;
-    await Receta.findByIdAndUpdate(this.receta, { calificacion_promedio: promedio });
-  }
-});*/
 
 module.exports = mongoose.model("Calificacion", calificacionSchema);
